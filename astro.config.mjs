ddd1f1c86b11ea415e8b78dace891cd9c934ea35
kibetnathan <kibetnat@gmail.com>
2026-07-07 13:58:41 +0300

    fix(seo): update seo files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gikomba.app",
   output: "server",
   adapter: netlify(),
   vite: {
